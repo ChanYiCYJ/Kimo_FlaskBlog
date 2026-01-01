9f7dd17a5ff7079a6b828c08ada4291f7749ff8b
ChanYiCYJ <j498780@gmail.com>
2026-01-02 00:05:27 +0800

    添加分类与标签功能，争取在这个星期写好一键配置功能

--- Kimo/views/archive.py
+++ Kimo/views/archive.py
@@ -9,11 +9,13 @@ bg=Blueprint('archive',__name__)
 @bg.route('/',methods=['GET','POST'])
 def index():
     dsb = session.get('user_role')
-    post = db.fetchall('select * from blog')
+    post = db.fetchall('select * from archives')or []
+    category_all = db.fetchall('select * from categories ', )or []
+    tag_all = db.fetchall('select * from tags ', )or []
     config =load_config('app','config')
     if request.method=='GET':
         return render_template('index.html', dashboard=dsb, page_title=config["title"],
-                               page_subtitle=config["introduction"], config=config, posts=post)
+                               page_subtitle=config["introduction"], config=config, posts=post,categorys=category_all,tags=tag_all)
 
 
     return post
@@ -21,7 +23,7 @@ def index():
 @bg.route('/archive/<string:archive_title>',methods=['GET','POST'])
 def archive(archive_title):
 
-    archive_page = db.fetch_one('select * from blog where title=%s', [archive_title, ])
+    archive_page = db.fetch_one('select * from archives where title=%s', [archive_title, ])
     if request.method=='GET':
         content = markdown.markdown(
     archive_page['content'],
@@ -43,6 +45,7 @@ def archive(archive_title):
     return archive
 
 
+
 @bg.route('/post', methods=['GET', 'POST'])
 def archive_post():
     check_user = session.get('user_role')
@@ -60,7 +63,7 @@ def archive_post():
                 return jsonify({'message': '内容为空'}), 400
 
             try:
-                db.implement('insert into blog(title,content) values (%s,%s)', [title, content])
+                db.implement('insert into archives(title,content) values (%s,%s)', [title, content])
             except Exception as e:
                 return jsonify({'message': str(e)}), 500
 
@@ -68,14 +71,30 @@ def archive_post():
 
     return '无权访问'
 
+@bg.route('/tags', methods=['GET', 'POST'])
+def tag():
+        tag_all = db.fetchall('select * from tags ', )
+        if request.method == 'GET':
+            config = load_config('app', 'config')
+            return render_template('tag.html', tag_all=tag_all,config=config)
+        return render_template('tag.html', tag_all=tag_all)
 
-@bg.route('/edit', methods=['POST'])
-def archive_edit():
+@bg.route('/category', methods=['GET', 'POST'])
+def category():
+    category_all = db.fetchall('select * from categories ', )
+    if request.method == 'GET':
+        config = load_config('app', 'config')
+        return render_template('category.html', category_all=category_all,config=config)
+    return render_template('category.html', category_all=category_all)
+
+
+@bg.route('/getpost', methods=['POST'])
+def get_post():
     check_user = session.get('user_role')
     if check_user == 0:
         if request.method == 'POST':
             post_id = request.json.get('post_id')
-            result = db.fetch_one('select * from blog where id=%s', [post_id, ])
+            result = db.fetch_one('select * from archives where id=%s', [post_id, ])
             if result:
                 return result
             return jsonify({'message': '没有这篇文章'}), 400
@@ -87,9 +106,9 @@ def archive_delete():
     check_user = session.get('user_role')
     if check_user == 0:
         post_id = request.json.get('post_id')
-        check = db.fetch_one('select * from blog where id=%s', [post_id, ])
+        check = db.fetch_one('select * from archives where id=%s', [post_id, ])
         if check:
-            db.implement('delete from blog where id=%s', [post_id, ])
+            db.implement('delete from archives where id=%s', [post_id, ])
             return jsonify({'message': '删除成功'})
 
         return jsonify({'message': '没有这篇文章'}), 400
